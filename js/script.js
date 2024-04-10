b33999c16c3c8f28176c3a2d4555c251f498840f
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-04-10 18:23:56 +0000

    math

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Fetuha
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume.
+ */
+function calculateCelsius() {
+  // input
+  const fahrenheit = parseInt(document.getElementById('fahrenheit').value)
+
+
+  // process
+  const celsius = 5.0 / 9.0* (fahrenheit-32)
+
+  // output
+  document.getElementById("celsius").innerHTML = 'Volume is: ' + celsius + '<sup> 0' 
+}
